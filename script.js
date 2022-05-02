6c7d2ca34c45c49760b2b285cd9ca83600b51cd7
LAPTOP-TJ2LU25K\Rebek <rebekah.kokaev@hotmail.com>
2022-05-02 12:21:55 +0200

    adding javascript and final nav functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const MENU_OPEN_BUTTON = document.querySelector('.open');
+const MENU_CLOSE_BUTTON = document.querySelector('.close');
+const SIDE_NAV = document.querySelector('.mobile-display');
+const BUTTON_FEATURES = document.querySelector('.features-button');
+const BUTTON_COMPANY = document.querySelector('.company-button');
+const SUB_OPTIONS_FEATURES = document.querySelector('.features');
+const SUB_OPTIONS_COMPANY = document.querySelector('.company')
+
+
+
+
+MENU_OPEN_BUTTON.addEventListener('click', menuController);
+MENU_CLOSE_BUTTON.addEventListener('click', menuController);
+
+BUTTON_FEATURES.addEventListener('click', featuresButtonController);
+BUTTON_COMPANY.addEventListener('click', companyButtonController);
+
+function menuController() {
+    SIDE_NAV.classList.toggle('hidden');
+}
+
+function featuresButtonController() {
+    let arrow = BUTTON_FEATURES.querySelector('img');
+    SUB_OPTIONS_FEATURES.classList.toggle('hidden');
+    arrow.classList.toggle('rotated');
+}
+
+function companyButtonController() {
+    let arrow = BUTTON_COMPANY.querySelector('img');
+    SUB_OPTIONS_COMPANY.classList.toggle('hidden');
+    arrow.classList.toggle('rotated');
+}
